460381ac90bb464f01754970646c0e5f8949d6ce
xuema <1461637280@qq.com>
2025-12-29 15:45:18 +0800

    add config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+EMAIL_USER = "xuemengm7@163.com"
+EMAIL_PASS = "RZWmkczvtXzkrJDx"  #有效期180天 202606
+EMAIL_TO = "xuemengm7@163.com"
+
+SMTP_SERVER = "smtp.163.com"
+SMTP_PORT = 465
+
+OUTPUT_EXCEL = "strategy_result.xlsx"
+HOT_INDUSTRY_TOPN = 15
+SCORE_THRESHOLD = 0.7
